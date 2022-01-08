58d7c29a285f5b050e176d64ecb2aa1b2ca4bb39
marahmanjs <imarahmandev@gmail.com>
2022-01-08 17:57:19 +0500

    added gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,19 @@
 var express = require('express');
+var axios = require('axios')
 var router = express.Router();
 
 const { isLoggedIn} = require('./../middlewares/auth')
 
-let users = require('./../data/users.json')
 
 /* GET home page. */
-router.get('/',isLoggedIn, function(req, res, next) {
-  if(req.cookies['auth_status'] == 'login'){
-
-    res.render('index', { title: 'Employee List', users});
-  }else{
-    req.flash('error' , 'Please Login First')
-    res.redirect('/login')
-  }
+router.get('/',isLoggedIn, async function(req, res, next) {
+  let response = await axios.get('https://61d979dece86530017e3cb27.mockapi.io/users')
+  res.render('index', { title: 'Employee List', users: response.data});
 });
 
-router.get('/users' ,isLoggedIn, function(req, res, next) {
-  res.json(users)
+router.get('/users' ,isLoggedIn, async function(req, res, next) {
+  let response = await axios.get('https://61d979dece86530017e3cb27.mockapi.io/users')
+  res.json(response.data)
 })
 
 module.exports = router;
